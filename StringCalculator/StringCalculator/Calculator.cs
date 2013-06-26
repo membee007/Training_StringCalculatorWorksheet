43ec299465021b5aad3130edc4149d8a2ff5a988
Mark Pearl <markpearl@gmail.com>
2013-06-26 09:21:34 +0200

    Added initial project

---
+++ StringCalculator/StringCalculator/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+    }
+
+    public class CalculatorTests
+    {
+
+    }
+}
